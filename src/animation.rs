2a87fe66078b542c354b051551a1531d97b0672d
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-06-05 15:23:14 +0000

    basic yaml parsing

--- src/animation.rs
+++ src/animation.rs
@@ -1,17 +1,26 @@
-use std::ops::RangeInclusive;
-use std::time::Duration;
+use std::{ops::RangeInclusive, time::Duration};
+
+#[cfg(feature = "unstable-load-from-file")]
+use std::{
+    error::Error,
+    fmt::{Display, Formatter},
+};
 
 use bevy_reflect::TypeUuid;
+#[cfg(feature = "unstable-load-from-file")]
+use serde::{de, Deserialize, Deserializer};
 
 /// Asset that define an animation of `TextureAtlasSprite`
 ///
 /// See crate level documentation for usage
 #[derive(Debug, Clone, Default, TypeUuid)]
+#[cfg_attr(feature = "unstable-load-from-file", derive(Deserialize))]
 #[uuid = "6378e9c2-ecd1-4029-9cd5-801caf68517c"]
 pub struct SpriteSheetAnimation {
     /// Frames
     pub(crate) frames: Vec<Frame>,
     /// Animation mode
+    #[cfg_attr(feature = "unstable-load-from-file", serde(default))]
     pub(crate) mode: Mode,
 }
 
@@ -38,12 +47,44 @@ pub enum AnimationMode {
 }
 
 /// A single animation frame
-#[derive(Debug, Copy, Clone, Default)]
+#[derive(Debug, Copy, Clone, Default, PartialEq, Eq)]
+#[cfg_attr(feature = "unstable-load-from-file", derive(Deserialize))]
 pub struct Frame {
     /// Index in the sprite atlas
     pub(crate) index: usize,
     /// How long should the frame be displayed
+    #[cfg_attr(
+        feature = "unstable-load-from-file",
+        serde(deserialize_with = "deserialize_duration")
+    )]
     pub(crate) duration: Duration,
+}
+
+#[cfg(feature = "unstable-load-from-file")]
+fn deserialize_duration<'de, D>(deserializer: D) -> Result<Duration, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    deserializer.deserialize_u64(DurationVisitor)
+}
+
+#[cfg(feature = "unstable-load-from-file")]
+struct DurationVisitor;
+
+#[cfg(feature = "unstable-load-from-file")]
+impl<'de> de::Visitor<'de> for DurationVisitor {
+    type Value = Duration;
+
+    fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
+    where
+        E: de::Error,
+    {
+        Ok(Duration::from_millis(v))
+    }
+
+    fn expecting(&self, formatter: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(formatter, "a positive integer")
+    }
 }
 
 impl SpriteSheetAnimation {
@@ -123,9 +164,35 @@ impl SpriteSheetAnimation {
     pub(crate) fn has_frames(&self) -> bool {
         !self.frames.is_empty()
     }
+
+    /// Parse content of a yaml file representing the animation
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the content is not a valid yaml representation of an animation
+    #[cfg(feature = "unstable-load-from-file")]
+    pub fn from_yaml(yaml: &str) -> Result<Self, AnimationParseError> {
+        serde_yaml::from_str(yaml).map_err(AnimationParseError)
+    }
 }
 
+#[cfg(feature = "unstable-load-from-file")]
+#[derive(Debug)]
+#[non_exhaustive]
+pub struct AnimationParseError(serde_yaml::Error);
+
+#[cfg(feature = "unstable-load-from-file")]
+impl Display for AnimationParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Animation format is invalid: {}", self.0)
+    }
+}
+
+#[cfg(feature = "unstable-load-from-file")]
+impl Error for AnimationParseError {}
+
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
+#[cfg_attr(feature = "unstable-load-from-file", derive(Deserialize))]
 pub(crate) enum Mode {
     Once,
     RepeatFrom(usize),
@@ -180,5 +247,37 @@ mod tests {
     #[should_panic]
     fn panics_for_zero_duration() {
         let _ = Frame::new(0, Duration::ZERO);
+    }
+
+    #[cfg(feature = "unstable-load-from-file")]
+    mod yaml_parsing {
+        use super::*;
+
+        #[test]
+        fn load_from_yaml() {
+            // given
+            let content = "
+            frames:
+              - index: 0
+                duration: 100 # ms
+              - index: 1
+                duration: 100 # ms
+              - index: 2
+                duration: 120 # ms";
+
+            // when
+            let animation = SpriteSheetAnimation::from_yaml(content).unwrap();
+
+            // then
+            assert_eq!(animation.mode, Mode::RepeatFrom(0));
+            assert_eq!(
+                animation.frames,
+                vec![
+                    Frame::new(0, Duration::from_millis(100)),
+                    Frame::new(1, Duration::from_millis(100)),
+                    Frame::new(2, Duration::from_millis(120)),
+                ]
+            );
+        }
     }
 }
