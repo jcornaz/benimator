762bf3e42aa5044531e81f8684606a6e919e877c
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-06-05 18:25:06 +0000

    add asset loader

---
+++ src/animation/load.rs
@@ -0,0 +1,22 @@
+use bevy_asset::{AssetLoader, LoadContext, LoadedAsset};
+use bevy_utils::BoxedFuture;
+
+use super::SpriteSheetAnimation;
+
+impl AssetLoader for SpriteSheetAnimation {
+    fn load<'a>(
+        &'a self,
+        bytes: &'a [u8],
+        load_context: &'a mut LoadContext<'_>,
+    ) -> BoxedFuture<'a, Result<(), anyhow::Error>> {
+        Box::pin(async move {
+            let custom_asset = SpriteSheetAnimation::from_yaml_bytes(bytes)?;
+            load_context.set_default_asset(LoadedAsset::new(custom_asset));
+            Ok(())
+        })
+    }
+
+    fn extensions(&self) -> &[&str] {
+        &[".animation.yml", ".animation.yaml"]
+    }
+}
