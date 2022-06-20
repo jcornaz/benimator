b5288acd3e6d99fddf136443e8b4a62d5a66e752
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-06-20 20:16:37 +0000

    feat(unstable-load-from-file): load animation files with `.ron` extension

--- src/animation/load.rs
+++ src/animation/load.rs
@@ -12,13 +12,17 @@ impl AssetLoader for SpriteSheetAnimationLoader {
         load_context: &'a mut LoadContext<'_>,
     ) -> BoxedFuture<'a, Result<(), anyhow::Error>> {
         Box::pin(async move {
-            let custom_asset = SpriteSheetAnimation::from_yaml_bytes(bytes)?;
+            let custom_asset = match load_context.path().extension().unwrap().to_str().unwrap() {
+                "yaml" | "yml" => SpriteSheetAnimation::from_yaml_bytes(bytes)?,
+                "ron" => SpriteSheetAnimation::from_ron_bytes(bytes)?,
+                _ => unreachable!(),
+            };
             load_context.set_default_asset(LoadedAsset::new(custom_asset));
             Ok(())
         })
     }
 
     fn extensions(&self) -> &[&str] {
-        &["animation.yml", "animation.yaml"]
+        &["animation.yml", "animation.yaml", "animation.ron"]
     }
 }
