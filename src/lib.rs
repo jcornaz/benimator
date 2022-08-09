e3b75d90916d66248fc09926c69496bd26faa118
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-08-09 22:46:36 +0200

    docs: improve crate-level example

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@
 
 //! A sprite animation library for rust game development
 //!
-//! Initially designed for [bevy](https://bevyengine.org), it is now engine agnostic.                       
+//! Initially designed for [bevy], it is now engine agnostic.                       
 //!
 //! # Get started
 //!
@@ -20,22 +20,27 @@
 //! each frame defined by an index.
 //!
 //! ```
-//! use std::time::Duration;
-//! use benimator::{Animation, FrameRate, State};
+//! # use std::time::Duration;
+//! use benimator::*;
 //!
 //! // Create an animation
 //! let animation = Animation::from_indices(0..=3, FrameRate::from_fps(10.0));
 //!
-//! // Get a new animation state
-//! let mut state = State::default();
+//! // Create a new animation state
+//! let mut state = State::new();
 //!
-//! // At each game-loop update, tell the state how much time has elapsed
+//! // In the game loop, for each update, tell the state how much time has elapsed
 //! let delta_time = Duration::from_millis(250);
 //! state.update(&animation, delta_time);
 //!
-//! // Then we can get the current frame index in order to render the right sprite
+//! // Then get the current frame index.
+//! // (so that we can tell our engine to render the sprite at that index)
 //! assert_eq!(state.frame_index(), 2);
 //! ```
+//!
+//! Have a look at the [examples](https://github.com/jcornaz/benimator/tree/main/examples) for complete examples using the [bevy] game engine.
+//!
+//! [bevy]: https://bevyengine.org
 
 #[cfg(test)]
 #[macro_use]
